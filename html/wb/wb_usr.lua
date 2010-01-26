dfe7bd51f70c9c449401843b99a974e7e51e2143
scuri <scuri>
2010-01-26 15:54:26 +0000

    *** empty log message ***

--- html/wb/wb_usr.lua
+++ html/wb/wb_usr.lua
@@ -12,7 +12,7 @@ wb_usr = {
 
 wb_usr.messages = {
   en = {
-    bar_title = "CD - Version 5.2",
+    bar_title = "CD - Version 5.3",
     title = "CD - Canvas Draw",
   }
 } 
--- include/cd.h
+++ include/cd.h
@@ -18,10 +18,10 @@ extern "C" {
 
 #define CD_NAME "CD - Canvas Draw"
 #define CD_DESCRIPTION "A 2D Graphics Library"
-#define CD_COPYRIGHT    "Copyright (C) 1994-2009 Tecgraf, PUC-Rio."
-#define CD_VERSION "5.2"              /* bug fixes are reported only by cdVersion functions */
-#define CD_VERSION_NUMBER 502000
-#define CD_VERSION_DATE "2009/06/26"
+#define CD_COPYRIGHT    "Copyright (C) 1994-2010 Tecgraf, PUC-Rio."
+#define CD_VERSION "5.3"              /* bug fixes are reported only by cdVersion functions */
+#define CD_VERSION_NUMBER 503000
+#define CD_VERSION_DATE "2010/01/26"
 
 typedef struct _cdContext cdContext;
 typedef struct _cdCanvas cdCanvas;
@@ -472,7 +472,7 @@ typedef int(*cdSizeCB)(cdCanvas *canvas, int w, int h, double w_mm, double h_mm)
 
 
 /******************************************************************************
-Copyright (C) 1994-2009 Tecgraf, PUC-Rio.
+Copyright (C) 1994-2010 Tecgraf, PUC-Rio.
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
