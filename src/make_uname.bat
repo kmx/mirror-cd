43a1ea611a2dbd2b0945c689e324dfab041e8766
scuri <scuri>
2010-11-10 00:59:23 +0000

    *** empty log message ***

--- src/make_uname.bat
+++ src/make_uname.bat
@@ -8,9 +8,6 @@ call tecmake %1 "USE_GDK=Yes" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cd_pdflib" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdpdf" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdgl" %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "MF=cdlua3" %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "MF=cdluapdf3" %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "MF=cdluagl3" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdlua5" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdluapdf5" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdluagl5" %2 %3 %4 %5 %6 %7 %8
@@ -22,3 +19,13 @@ call tecmake %1 "MF=cdcontextplus" %2 %3 %4 %5 %6
 call tecmake %1 "MF=cdluacontextplus5" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdcairo" %2 %3 %4 %5 %6
 call tecmake %1 "MF=cdluacairo5" %2 %3 %4 %5 %6 %7 %8
+
+if defined TECGRAF_INTERNAL goto tec_internal
+goto end
+
+:tec_internal
+call tecmake %1 "MF=cdlua3" %2 %3 %4 %5 %6 %7 %8
+call tecmake %1 "MF=cdluapdf3" %2 %3 %4 %5 %6 %7 %8
+call tecmake %1 "MF=cdluagl3" %2 %3 %4 %5 %6 %7 %8
+
+:end
