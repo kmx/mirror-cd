28dd2041b33b8afb272c057e8207cb941c936b66
scuri <scuri>
2011-02-13 16:21:05 +0000

    *** empty log message ***

--- include/cd.h
+++ include/cd.h
@@ -18,7 +18,7 @@ extern "C" {
 
 #define CD_NAME "CD - Canvas Draw"
 #define CD_DESCRIPTION "A 2D Graphics Library"
-#define CD_COPYRIGHT    "Copyright (C) 1994-2010 Tecgraf, PUC-Rio."
+#define CD_COPYRIGHT    "Copyright (C) 1994-2011 Tecgraf, PUC-Rio."
 #define CD_VERSION "5.4"              /* bug fixes are reported only by cdVersion functions */
 #define CD_VERSION_NUMBER 504000
 #define CD_VERSION_DATE "2010/06/24"  /* does not include bug fix releases */
@@ -502,7 +502,7 @@ enum {
 
 
 /******************************************************************************
-Copyright (C) 1994-2010 Tecgraf, PUC-Rio.
+Copyright (C) 1994-2011 Tecgraf, PUC-Rio.
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
